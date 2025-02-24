430204bb50b59db255426ef4d57fa6acb5a236fe
nikesh <nikeshdev0@gmail.com>
2025-02-25 00:32:34 +0545

    feat: add server and client streaming for GreetService

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	greet "github.com/NikeshSapkota01/learningGrpc/greet/greetpb"
 	"google.golang.org/grpc"
@@ -14,8 +15,7 @@ type server struct {
 	greet.UnimplementedGreetServiceServer
 }
 
-// func (s *server) MethodName(ctx context.Context, req *pb.RequestType) (*pb.ResponseType, error)
-
+// Greet method for unary RPC
 func (*server) Greet(ctx context.Context, req *greet.GreetRequest) (*greet.GreetResponse, error) {
 	fmt.Printf("greet %v\n", req.String())
 	fn := req.GetFirstName()
@@ -27,6 +27,29 @@ func (*server) Greet(ctx context.Context, req *greet.GreetRequest) (*greet.Greet
 	}
 
 	return res, nil
+}
+
+// ServerGreetStream method for server streaming RPC
+func (*server) ServerGreetStream(req *greet.ServerGreetStreamRequest, stream greet.GreetService_ServerGreetStreamServer) error {
+	fmt.Printf("ServerGreetStream function called with: %v\n", req.String())
+	fn := req.GetFirstName()
+	ln := req.GetLastName()
+
+	// Send 5 streaming responses
+	for i := 0; i < 5; i++ {
+		res := &greet.ServerGreetStreamResponse{
+			Message: fmt.Sprintf("Hello, %s %s! Message %d", fn, ln, i+1),
+		}
+
+		if err := stream.Send(res); err != nil {
+			return fmt.Errorf("failed to send message: %v", err)
+		}
+
+		// Simulate some delay between messages
+		time.Sleep(1 * time.Second)
+	}
+
+	return nil
 }
 
 func main() {
